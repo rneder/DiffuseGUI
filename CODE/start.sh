f91ec362f2db85e25d2e36a08d8360c337bd3fca
Reinhard <reinhard.neder@fau.de>
2017-02-17 16:12:55 +0100

    Corrected secondary window line

--- CODE/start.sh
+++ CODE/start.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 if( /bin/ps | /bin/grep XWin ); then
    export DISPLAY=':0';
-   /bin/xterm -rightbar -sb -pob -title "Discus_Suite secondary window" -e /bin/discus_suite_run.sh;
+   /bin/xterm -rightbar -sb -pob -title "Discus_Suite secondary window" -e /bin/python3 suite_gui.py;
 else
    /bin/rm -f /tmp/.X*-lock
    /bin/xinit "/bin/xterm" -rightbar -sb -pob -title "Discus_Suite primary window exit last" -e /bin/python3 suite_gui.py -- "/usr/bin/XWin" :0 -multiwindow -logfile /dev/null;
 fi
-###-auth '/home/neder/.serverauth.7200'
